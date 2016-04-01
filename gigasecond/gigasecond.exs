7173cb886375945235329835ef681a5a040f4518
Andrei Chernykh <andrei.chernykh@gmail.com>
2016-04-01 11:36:36 +0300

    indentation

--- gigasecond/gigasecond.exs
+++ gigasecond/gigasecond.exs
@@ -1,14 +1,14 @@
 defmodule Gigasecond do
 
-	@gigasecond :math.pow(10, 9) |> round
+  @gigasecond :math.pow(10, 9) |> round
 
-	@doc """
-	Calculate a date one billion seconds after an input date.
-	"""
-	@spec from({{pos_integer, pos_integer, pos_integer}, {pos_integer, pos_integer, pos_integer}}) :: :calendar.datetime
-	# {{year, month, day}, {hours, minutes, seconds}} = Erlang :calendar.datetime
-	def from(datetime) do
-		in_seconds = :calendar.datetime_to_gregorian_seconds(datetime)
-		:calendar.gregorian_seconds_to_datetime(in_seconds + @gigasecond)
-	end
+  @doc """
+  Calculate a date one billion seconds after an input date.
+  """
+  @spec from({{pos_integer, pos_integer, pos_integer}, {pos_integer, pos_integer, pos_integer}}) :: :calendar.datetime
+  # {{year, month, day}, {hours, minutes, seconds}} = Erlang :calendar.datetime
+  def from(datetime) do
+    in_seconds = :calendar.datetime_to_gregorian_seconds(datetime)
+    :calendar.gregorian_seconds_to_datetime(in_seconds + @gigasecond)
+  end
 end
